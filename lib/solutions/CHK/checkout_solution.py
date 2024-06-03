5441c26121c3b398890be6bba672aa9faf8e40ae
sbx_user1051 <sbx_user1051@169.254.83.197>
2024-06-03 16:36:13 +0000

    Mon Jun 03 16:36:13 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,9 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
-
+"""
+ As mentioned below, constants should be in seperate file, but to make it easier to
+ Review I've left it here
+"""
 KNOWN_SKUS = ["A", "B", "C", "D"]
 
 def checkout(skus):
@@ -27,6 +28,7 @@ def checkout(skus):
         basket.scan(sku)
 
 class Item():
+    # Init function to set default values for child objects
     def __init__(self):
         self.total_cost = 0
         self.single_cost = 0
@@ -34,15 +36,23 @@ class Item():
         self.discount_quantity = 0
         self.discount_amount = 0
 
+    # Cost function rings up the value of the quantity and cost of each item
     def cost(self):
         self.total_cost = self.single_cost * self.quantity
+        self.discount()
 
+    # Discount function calculates how many applicable discounts are available and reduces cost
     def discount(self):
         valid_discounts = self.quantity // self.discount_quantity
+        self.total_cost = self.total_cost - self.discount_amount * valid_discounts
 
+    # Scan adds another quantity of an item to the basket
     def scan(self):
         self.quantity += 1
 
+"""
+Below are definitions for individual items, their costing and their discounts. 
+"""
 class SKU_A(Item):
     def __init__(self):
         super.__init__()
@@ -67,12 +77,15 @@ class SKU_D(Item):
         super.__init__()
         self.single_cost = 15
 
-# Ideally this and other constants would be in another file but for simplicity of review I'll
-# leave it like this. KNOWN_SKUS wouldn't need to exist since  SKU_ITEM_MAP.keys() would work.
+"""
+ Ideally this and other constants would be in another file but for simplicity of review I'll
+ leave it like this. KNOWN_SKUS wouldn't need to exist since  SKU_ITEM_MAP.keys() would work.
+"""
 SKU_ITEM_MAP = {"A": SKU_A, "B": SKU_B, "C": SKU_C, "D": SKU_D}
 
 
 class Basket():
+    # Init holds the baskets contents as singleton objects with quantity, value and cost values
     def __init__(self):
         self.items = {}
 
@@ -84,6 +97,13 @@ class Basket():
         else:
             self.items[sku] = SKU_ITEM_MAP[sku]()
 
+    # Calculates total cost of items in basket
     def checkout(self):
-        pass
+        total_cost = 0
+        for item in self.items.values():
+            item.cost()
+            total_cost += item.total_cost
+
+        return total_cost
+
 
---
+++ test/solution_tests/CHK/test_checkout.py
@@ -0,0 +1,14 @@
+from solutions.SUM import sum_solution
+
+
+class TestSum():
+    def test_sum(self):
+        assert sum_solution.compute(1, 2) == 3
+
+    def test_sum_one(self):
+        assert sum_solution.compute(5, 5) == 10
+
+    def test_sum_two(self):
+        assert sum_solution.compute(10000, 10) == 10010
+
+
