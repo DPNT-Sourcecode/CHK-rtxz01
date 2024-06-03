f0e188a57453ab045970e516ae6d287614c6b055
sbx_user1051 <sbx_user1051@169.254.83.197>
2024-06-03 16:46:40 +0000

    Mon Jun 03 16:46:40 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -8,6 +8,10 @@ class TestSum():
         assert checkout_solution.checkout(["A", "B", True, "C"]) == -1
         assert checkout_solution.checkout("ABCD") == 115
         assert checkout_solution.checkout("AAA") == 130
+        assert checkout_solution.checkout("BBBB") == 90
+        assert checkout_solution.checkout("BBAAABB") == 220
+        assert checkout_solution.checkout("CD") == 35
+
 
 
 
