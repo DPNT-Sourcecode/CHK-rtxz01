965919cef4ecb90aa44280b1070b6abf515a7700
sbx_user1051 <sbx_user1051@169.254.83.197>
2024-06-03 16:24:13 +0000

    Mon Jun 03 16:24:13 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,20 +6,74 @@
 KNOWN_SKUS = ["A", "B", "C", "D"]
 
 def checkout(skus):
-    raise NotImplementedError()
+    # Verify all items in sku string are valid strings and known
+    # Check if input string is a string
+    try:
+        str(skus)
+    except Exception:
+            return -1
 
-class Basket():
-    def __init__(self):
-        self.items = []
+    # Check all letters inside the string are valid and letters
+    for sku in skus:
+        if sku not in KNOWN_SKUS:
+            return -1
+        try:
+            str(sku)
+        except Exception:
+            return -1
 
-    # Method for adding new item to basket
-    # - param sku = letter code of item SKU that has been scanned
-    def scan(self, sku):
-        if sku.upper not in
+    basket = Basket()
+    for sku in skus:
+        basket.scan(sku)
 
 class Item():
+    def __init__(self):
+        self.single_cost = 0
+        self.quantity = 0
+        self.discount_quantity = 0
+        self.discount_amount = 0
+
     def cost(self):
         raise NotImplementedError()
 
     def discount(self):
         raise NotImplementedError()
+
+    def scan(self):
+        raise NotImplementedError()
+
+class SKU_A(Item):
+    def __init__(self):
+        super.__init__()
+        self.single_cost = 50
+        self.discount_quantity = 3
+        self.discount_amount = 
+
+class SKU_B(Item):
+    pass
+
+class SKU_C(Item):
+    pass
+
+class SKU_D(Item):
+    pass
+
+# Ideally this and other constants would be in another file but for simplicity of review I'll
+# leave it like this. KNOWN_SKUS wouldn't need to exist since  SKU_ITEM_MAP.keys() would work.
+SKU_ITEM_MAP = {"A": SKU_A, "B": SKU_B, "C": SKU_C, "D": SKU_D}
+
+
+class Basket():
+    def __init__(self):
+        self.items = {}
+
+    # Method for adding new item to basket
+    # - param sku = letter code of item SKU that has been scanned
+    def scan(self, sku):
+        if sku in self.items.keys():
+            self.items[sku].scan()
+        else:
+            self.items[sku] = SKU_ITEM_MAP[sku]()
+
+    def checkout(self):
+        pass
