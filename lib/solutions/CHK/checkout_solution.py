4ba3003993024dc5b2809130fb67fcff861ea453
sbx_user1051 <sbx_user1051@169.254.83.197>
2024-06-03 16:09:13 +0000

    Mon Jun 03 16:09:13 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,13 +2,25 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+KNOWN_SKUS = ["A", "B", "C", "D"]
+
 def checkout(skus):
     raise NotImplementedError()
 
+class Basket():
+    def __init__(self):
+        self.items = []
+
+    # Method for adding new item to basket
+    # - param sku = letter code of item SKU that has been scanned
+    def scan(self, sku):
+        if sku.upper not in
 
 class Item():
     def cost(self):
         raise NotImplementedError()
 
-    
+    def discount(self):
+        raise NotImplementedError()
 
