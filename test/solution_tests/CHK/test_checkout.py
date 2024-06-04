6f8eb547fbb8db5de7406e758fb1b7570cded184
sbx_user1051 <sbx_user1051@169.254.134.117>
2024-06-04 18:43:32 +0000

    Tue Jun 04 18:43:32 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -79,13 +79,13 @@ class TestSum():
         # buy x get y free tests
         assert checkout_solution.checkout("FFF") == 20
         assert checkout_solution.checkout("FFFF") == 30
-        assert checkout_solution.checkout("FFFFF") == 30
+        assert checkout_solution.checkout("FFFFF") == 40
         assert checkout_solution.checkout("FFFFFF") == 40
         assert checkout_solution.checkout("UUU") == 120
         assert checkout_solution.checkout("UUUU") == 120
-        assert checkout_solution.checkout("UUUUU") == 170
+        assert checkout_solution.checkout("UUUUU") == 160
         assert checkout_solution.checkout("UUUUUU") == 240
-        assert checkout_solution.checkout("UUUUUUU") == 240
+        assert checkout_solution.checkout("UUUUUUU") == 200
         assert checkout_solution.checkout("UUUUUUUU") == 240
 
 
@@ -121,6 +121,7 @@ track of my mistakes here.
         # assert checkout_solution.checkout("ABCDECBAABCABBAAAEEAA") == 665  # Got 695
         # 455 vs 470 result means I'm not calculating the 2B discount properly
         # This was due to a silly error in a =- b rather than  a = a - b, oops, should have function tested that!
+
 
 
 
