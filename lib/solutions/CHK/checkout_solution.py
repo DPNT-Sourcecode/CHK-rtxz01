cca087e948207d952f36e0896ee127aac314cd39
sbx_user1051 <sbx_user1051@169.254.102.53>
2024-06-05 17:33:00 +0000

    Wed Jun 05 17:33:00 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -25,31 +25,32 @@ GD_3STXYZ_for_45 = (3, 45, "STXYZ")
 SKU_DISCOUNT_MAP = {
     "A": {KEY_SINGLECOST: 50, KEY_MULTIBUY_OFFERS: [(3, 20), (5, 50)]},
     "B": {KEY_SINGLECOST: 30, KEY_MULTIBUY_OFFERS: [(2, 15)], KEY_MULTIPRICE_OFFERS: [(2, "E")]},
-    "C": {KEY_SINGLECOST: 20,},
-    "D": {KEY_SINGLECOST: 15,},
-    "E": {KEY_SINGLECOST: 40,},
+    "C": {KEY_SINGLECOST: 20, },
+    "D": {KEY_SINGLECOST: 15, },
+    "E": {KEY_SINGLECOST: 40, },
     "F": {KEY_SINGLECOST: 10, KEY_BUYX_GETY_FREE_OFFERS: [(2, 1)]},
-    "G": {KEY_SINGLECOST: 20,},
+    "G": {KEY_SINGLECOST: 20, },
     "H": {KEY_SINGLECOST: 10, KEY_MULTIBUY_OFFERS: [(5, 5), (10, 20)]},
-    "I": {KEY_SINGLECOST: 35,},
-    "J": {KEY_SINGLECOST: 60,},
+    "I": {KEY_SINGLECOST: 35, },
+    "J": {KEY_SINGLECOST: 60, },
     "K": {KEY_SINGLECOST: 70, KEY_MULTIBUY_OFFERS: [(2, 20)]},
-    "L": {KEY_SINGLECOST: 90,},
+    "L": {KEY_SINGLECOST: 90, },
     "M": {KEY_SINGLECOST: 15, KEY_MULTIPRICE_OFFERS: [(3, "N")]},
-    "N": {KEY_SINGLECOST: 40,},
-    "O": {KEY_SINGLECOST: 10,},
+    "N": {KEY_SINGLECOST: 40, },
+    "O": {KEY_SINGLECOST: 10, },
     "P": {KEY_SINGLECOST: 50, KEY_MULTIBUY_OFFERS: [(5, 50)]},
     "Q": {KEY_SINGLECOST: 30, KEY_MULTIBUY_OFFERS: [(3, 10)], KEY_MULTIPRICE_OFFERS: [(3, "R")]},
-    "R": {KEY_SINGLECOST: 50,},
+    "R": {KEY_SINGLECOST: 50, },
     "S": {KEY_SINGLECOST: 20, KEY_GROUP_DISCOUNT: GD_3STXYZ_for_45},
     "T": {KEY_SINGLECOST: 20, KEY_GROUP_DISCOUNT: GD_3STXYZ_for_45},
     "U": {KEY_SINGLECOST: 40, KEY_BUYX_GETY_FREE_OFFERS: [(3, 1)]},
     "V": {KEY_SINGLECOST: 50, KEY_MULTIBUY_OFFERS: [(2, 10), (3, 20)]},
-    "W": {KEY_SINGLECOST: 20,},
+    "W": {KEY_SINGLECOST: 20, },
     "X": {KEY_SINGLECOST: 17, KEY_GROUP_DISCOUNT: GD_3STXYZ_for_45},
     "Y": {KEY_SINGLECOST: 20, KEY_GROUP_DISCOUNT: GD_3STXYZ_for_45},
     "Z": {KEY_SINGLECOST: 21, KEY_GROUP_DISCOUNT: GD_3STXYZ_for_45},
 }
+
 
 def checkout(skus):
     # Verify all items in sku string are valid strings and known
@@ -73,6 +74,8 @@ def checkout(skus):
     total = basket.checkout()
     return total
 
+
+# Item class that will hold info about a given SKU, it's discounts and it's current qunatity and discounts
 class Item():
     # Init function to set default values for child objects
     def __init__(self, sku):
@@ -89,23 +92,25 @@ class Item():
     # Cost function rings up the value of the quantity and cost of each item
     def cost(self, all_items):
         group_discount_cost = self.get_and_apply_group_discount(all_items)
+
         buyX_getY_free_items = self.get_buyx_gety_free_quantity()
         self.quantity -= buyX_getY_free_items
+
         self.apply_best_multiprice_discount(all_items)
         best_multibuy_discount = self.get_best_multibuy_discount()
         unit_costs = self.single_cost * self.quantity
         self.total_cost = (unit_costs + group_discount_cost) - (best_multibuy_discount)
 
-
+    # Applies best multiprice, once found will reduce stock count
     # @ param - all-items = all items in basket
     def apply_best_multiprice_discount(self, all_items):
         """
         My understanding here based on the wording is that if a purchase is made of 'EEBB',
-        one of the B items is free, bu still counts towards the 2 for promo.
+        one of the B items is free, but still counts towards the 2 for promo.
         This would result in a price of 2E (80) + 2B (45) - multipriced (30).
-        Otherwise this would rqeirue checking if mulipriced discount is cheaper than multibuy.
+        Otherwise, this would rqeirue checking if mulipriced discount is cheaper than multibuy.
         This assumption is based on the wording of
-         "All the offers are well balanced so that they can be safely combined."
+         "All the offers are well-balanced so that they can be safely combined."
         """
         """
         After deploying and failing a test I learned that 4 EE purchases should remove the cost even the discounted
@@ -124,12 +129,12 @@ class Item():
         Realised here I was both decrementing the quantity and applying a cost discount
         """
 
-
+    # Gets the best multibuy discount and returns the value back to be costed
     # @returns best applicable multibuy discount
     def get_best_multibuy_discount(self):
         # Multibuy discount is only for discounts based on buying X amounts of product
         """
-        Some further coments to this below block, it is not 100% optimal, there may possibly be cases where
+        Some further comments to this below block, it is not 100% optimal, there may be cases where
         different combinations of multibuy works out to a better discount than just doing 2 passes.
         I thought this was suitable for the given complexity of the context. A shop wouldn't in my guess
         have super complicated rules of multibuy.
@@ -141,7 +146,6 @@ class Item():
             first_quantity, first_multibuy_discount = multibuy
             first_discount = first_multibuy_discount * (self.quantity // first_quantity)
             remainder_quantity = self.quantity % first_quantity
-            best_second_discount = 0
 
             for second_multibuy in self.multibuy_discount_offers:
                 second_quantity, second_mutlybuy_discount = second_multibuy
@@ -183,6 +187,7 @@ class Item():
 
         discount_quantity_required, discount_cost, discount_eligible_items = self.group_discount
         promo_items = [item for item in all_items.values() if item.sku in discount_eligible_items]
+
         # Sorts the list by price descending to make it easier to remove most expensive first
         promo_items.sort(key=lambda item: item.single_cost, reverse=True)
 
@@ -199,11 +204,10 @@ class Item():
         total_discount_cost = group_discounts_quantity * discount_cost
         return total_discount_cost
 
-
-
     # Scan adds another quantity of an item to the basket
     def scan(self):
         self.quantity += 1
+
 
 """
 Below are definitions for individual items, their costing and their discounts. 
@@ -214,39 +218,6 @@ But at this point it looks like it's redundant and should be refactored to a map
 and easier maintenance, if this changes I could always revert to this method.
 """
 
-class SKU_A(Item):
-    def __init__(self):
-        super().__init__()
-        self.single_cost = 50
-        self.multibuy_discount_offers = [(3, 20), (5, 50)]
-
-class SKU_B(Item):
-    def __init__(self):
-        super().__init__()
-        self.single_cost = 30
-        self.multibuy_discount_offers = [(2, 15)]
-        self.multiprice_one_free_if = [(2, "E")]
-
-class SKU_C(Item):
-    def __init__(self):
-        super().__init__()
-        self.single_cost = 20
-
-class SKU_D(Item):
-    def __init__(self):
-        super().__init__()
-        self.single_cost = 15
-
-class SKU_E(Item):
-    def __init__(self):
-        super().__init__()
-        self.single_cost = 40
-
-class SKU_F(Item):
-    def __init__(self):
-        super().__init__()
-        self.single_cost = 10
-        self.buyx_gety_free = [(2, 1)]
 
 class Basket():
     # Init holds the baskets contents as singleton objects with quantity, value and cost values
@@ -271,8 +242,11 @@ class Basket():
 
         return total_cost
 
+
+# Main function for running manual debugger
 if __name__ == "__main__":
     checkout("SSXYZ")
+
 
 
 
